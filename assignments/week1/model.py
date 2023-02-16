78f58450202c7983100b6084a01d3ebb18092a6b
Yesao <673392438@qq.com>
2023-02-15 20:13:42 -0500

    Assignment 1 & 2-Ericyi

--- assignments/week1/model.py
+++ assignments/week1/model.py
@@ -2,18 +2,60 @@ import numpy as np
 
 
 class LinearRegression:
+    """
+    The basic linear regression model using closed-form formula.
+    """
 
     w: np.ndarray
     b: float
 
-    def __init__(self):
-        raise NotImplementedError()
+    def __init__(self) -> None:
+        # Initializing the parameters
+        self.w = np.array([])
+        self.b = 0.0
 
-    def fit(self, X, y):
-        raise NotImplementedError()
+    def fit(self, X: np.ndarray, y: np.ndarray) -> None:
+        """
 
-    def predict(self, X):
-        raise NotImplementedError()
+        Using closed-form formula to fit a linear regression model
+
+        Args:
+            X: np.ndarray, input data
+            y: Target values
+
+        Returns: None
+
+        """
+
+        # Appending a column of ones in X to add the bias term.
+        X = np.hstack((np.ones((X.shape[0], 1)), X))
+
+        # Compute the transpose of X
+        X_T = X.T
+
+        # Compute X^T X
+        XT_X = X.T @ X
+
+        # Compute the inverse of X^T X
+        inv_XT_X = np.linalg.inv(XT_X)
+
+        # Compute the dot product of (X^T X)^-1 and X^T
+        self.w = inv_XT_X @ (X_T @ y)
+        self.b = self.w[0]
+        self.w = self.w[1:]
+
+    def predict(self, X: np.ndarray) -> np.ndarray:
+        """
+
+        Predict the output for the given input.
+
+        Args:
+            X: Input data
+
+        Returns: np.ndarray, output
+
+        """
+        return X @ self.w + self.b
 
 
 class GradientDescentLinearRegression(LinearRegression):
@@ -24,7 +66,43 @@ class GradientDescentLinearRegression(LinearRegression):
     def fit(
         self, X: np.ndarray, y: np.ndarray, lr: float = 0.01, epochs: int = 1000
     ) -> None:
-        raise NotImplementedError()
+        """
+
+        Args:
+            X: np.ndarray, The input data
+            y: Target values
+            lr: learning rate for gradient descent.
+            epochs: number of epochs
+
+        Returns: None
+
+        """
+
+        m, n = X.shape
+
+        self.w = np.zeros(n)
+
+        self.b = 0.0
+
+        # Implementing Gradient Descent
+        for i in range(epochs):
+            y_hat = X @ self.w + self.b
+
+            """
+                    We use Mean Squared Error Equation as our loss function,
+                    then calculate the partial derivative of the loss function.
+                    J(w) = 1/2m * (y_hat - y) ** 2, 1/2 is for simplification
+
+                    dJ/dy_hat = 1/m * (y_hat - y) and dy_hat/dw = X,
+                    so dJ/dw = dJ/dy_hat * dy_hat/dw = 1/m * (y_hat - y) @ X
+            """
+            # derived_w = (1 / m) * (X.T @ (y_hat - y))
+            derived_b = (1 / m) * np.sum(y_hat - y)
+            derived_w = 1 / m * (y_hat - y) @ X
+
+            # Updating parameters
+            self.w -= lr * derived_w
+            self.b -= lr * derived_b
 
     def predict(self, X: np.ndarray) -> np.ndarray:
         """
@@ -37,4 +115,4 @@ class GradientDescentLinearRegression(LinearRegression):
             np.ndarray: The predicted output.
 
         """
-        raise NotImplementedError()
+        return X @ self.w + self.b
--- assignments/week3/model.py
+++ assignments/week3/model.py
@@ -2,7 +2,11 @@ import torch
 from typing import Callable
 
 
-class MLP:
+class MLP(torch.nn.Module):
+    """
+    A simple MLP model with hidden layers, activation and initializer
+    """
+
     def __init__(
         self,
         input_size: int,
@@ -22,9 +26,32 @@ class MLP:
             activation: The activation function to use in the hidden layer.
             initializer: The initializer to use for the weights.
         """
-        ...
+        super(MLP, self).__init__()
 
-    def forward(self, x):
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.num_classes = num_classes
+
+        self.hidden_count = hidden_count
+        self.activation = activation()
+        self.initializer = initializer
+
+        # create the input layer
+        self.input_layer = torch.nn.Linear(self.input_size, self.hidden_size)
+        self.initializer(self.input_layer.weight)
+
+        # create hidden layers
+        self.hidden_layers = torch.nn.ModuleList()
+        for i in range(self.hidden_count):
+            layer = torch.nn.Linear(hidden_size, hidden_size)
+            self.initializer(layer.weight)
+            self.hidden_layers += [layer]
+
+        # create output layer
+        self.output_layer = torch.nn.Linear(hidden_size, num_classes)
+        self.initializer(self.output_layer.weight)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         """
         Forward pass of the network.
 
@@ -34,4 +61,17 @@ class MLP:
         Returns:
             The output of the network.
         """
-        ...
+
+        # pass input to input layer and activation function
+        x = self.input_layer(x)
+        x = self.activation(x)
+
+        # pass through the hidden layers
+        for layer in self.hidden_layers:
+            x = layer(x)
+            x = self.activation(x)
+
+        # pass through the output layer
+        x = self.output_layer(x)
+
+        return x
