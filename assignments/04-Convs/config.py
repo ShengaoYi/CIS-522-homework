95f0732b1fb4bc91abaf29bb61a5f870dc8609f5
Yesao <673392438@qq.com>
2023-03-06 10:42:56 -0500

    Assignment Convs - Ericyi

--- assignments/04-Convs/config.py
+++ assignments/04-Convs/config.py
@@ -2,15 +2,15 @@ from typing import Callable
 import torch
 import torch.optim
 import torch.nn as nn
-from torchvision.transforms import Compose, ToTensor
+from torchvision.transforms import Compose, ToTensor, Normalize
 
 
 class CONFIG:
     batch_size = 8
-    num_epochs = 8
+    num_epochs = 5
 
     optimizer_factory: Callable[
         [nn.Module], torch.optim.Optimizer
     ] = lambda model: torch.optim.Adam(model.parameters(), lr=1e-3)
 
-    transforms = Compose([ToTensor()])
+    transforms = Compose([ToTensor(), Normalize(mean=0.5, std=0.5)])
--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -1,9 +1,61 @@
 import torch
+import torch.nn as nn
 
 
 class Model(torch.nn.Module):
+    """
+    My Convolutional Neural Network
+    """
+
     def __init__(self, num_channels: int, num_classes: int) -> None:
-        ...
+        """
+
+        Initialise parameters of CNN
+
+        Args:
+            num_channels: Number of input channels
+            num_classes: Number of output classes
+        """
+
+        self.conv1 = nn.Conv2d(
+            in_channels=num_channels, out_channels=32, kernel_size=3, padding=1
+        )
+        self.conv2 = nn.Conv2d(
+            in_channels=32, out_channels=64, kernel_size=3, padding=1
+        )
+
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.fc1 = nn.Linear(64 * 32 * 32, 128)
+        self.fc2 = nn.Linear(128, num_classes)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        ...
+        """
+
+        Forward pass of CNN
+
+        Args:
+            x: Input
+
+        Returns: Output result
+
+        """
+
+        x = self.conv1(x)
+        x = nn.ReLU(x)
+
+        x = self.conv2(x)
+        x = nn.ReLU(x)
+
+        x = self.conv2(x)
+        x = nn.ReLU(x)
+
+        x = self.pool(x)
+        x = torch.flatten(x, start_dim=1)
+
+        x = self.fc1(x)
+        x = nn.ReLU(x)
+
+        x = self.fc2(x)
+
+        return x
